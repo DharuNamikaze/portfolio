95dffa051906bd4054c46f6c33ae1dd15b45efb2
DharuNamikaze <dharunamikaze@gmail.com>
2025-07-15 15:18:09 +0530

    project changes

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import FlowingMenu from './ui/FlowingMenu'
 const Projects = () => {
+
   const demoItems = [
     { link: 'https://convertify-rho.vercel.app/', text: 'Convertify', image: '' },
     { link: 'https://type-d.vercel.app/', text: 'Typed', image: '' },
-    { link: 'https://type-d.vercel.app/', text: 'Odyssey', image: '' },
+    { link: 'https://my-odyssey.vercel.app/', text: 'Odyssey', image: '' },
     { link: 'https://smart-pick-nu.vercel.app/', text: 'smartpick', image: '' },
-    { link: 'https://github.com/DharuNamikaze/codexp', text: 'Codexp', image: '' },
-    { link: 'https://github.com/DharuNamikaze/chroniclesofyou', text: 'ChroniclesOfYou', image:''}
+    { link: 'https://github.com/DharuNamikaze/chroniclesofyou', text: 'ChroniclesOfYou', image: '' },
+    { link: 'https://github.com/DharuNamikaze/cellsaver', text: 'CellSaver', image: '' }
   ];
 
   return (
